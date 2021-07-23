fcc0557a06fd1f26e8920b66d7693041d3fbd249
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-07-23 21:51:29 +0530

    • @Psycho_Bots •

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ class config(object):
    API_ID = int(os.environ.get("API_ID"))
    API_HASH = os.environ.get("API_HASH")
    TOKEN = os.environ.get("TOKEN")
-
+# bot.py USED THAT ( little easy description for fucking kanger)😂
 # Kanger = Gay
 # Kang = Madarchod
