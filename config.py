d738de7962dac8b897a29f6924036208b3b5794b
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-07-23 21:56:01 +0530

    For my termux

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os 
 class config(object): 
-   API_ID = int(os.environ.get("API_ID"))
-   API_HASH = os.environ.get("API_HASH")
-   TOKEN = os.environ.get("TOKEN")
+   API_ID = 2698821
+   API_HASH = "4af625c75eb9e72e1b228411b0c7cd42"
+   TOKEN = "1936245618:AAHP3W3jvIXl7kTBq6mta1I3OGHYfMhP5Cc"
 # bot.py USED THAT ( little easy description for fucking kanger)😂
 # Kanger = Gay
 # Kang = Madarchod
