39b1f5c778f3ad6280f1589975cb2693cca57986
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-07-23 16:29:28 +0530

    Updated ✓

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
-import os
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
-TOKEN = os.environ.get("TOKEN")
+import os 
+class config(object): 
+   API_ID = int(os.environ.get("API_ID"))
+   API_HASH = os.environ.get("API_HASH")
+   TOKEN = os.environ.get("TOKEN")
 
 # Kanger = Gay
 # Kang = Madarchod
